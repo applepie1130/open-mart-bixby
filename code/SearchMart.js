aee441b90a14ae2f54ba44c0a69ce9cb35024eeb
SungJun Kim <arduous1130@gmail.com>
2019-10-13 19:51:51 +0900

    initial commit

---
+++ code/SearchMart.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var console = require('console');
+var config = require('config');
+
+module.exports.function = function searchMart (name, myLocation) {
+  console.log("## Search Mart ##");
+
+  var latitude = myLocation.latitude;
+  var longitude = myLocation.longitude;
+
+  console.log("## parameters ##");
+  console.log(name);
+  console.log(latitude);
+  console.log(longitude);
+
+  var options = { 
+    format: 'json',
+    query: {
+      'latitude': latitude,
+      'longitutde': longitude,
+      'name': name
+    }
+  };
+
+  // Call API
+  var response = http.getUrl(config.get('remote.url') + '/v1/mart/martHolidayInfos', options);  
+  return response;
+}
